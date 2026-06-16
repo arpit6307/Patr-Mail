7bc5fbf17a8e3354355a5f5a48de5f9910ba930c
Arpit Singh Yadav <arpitsinghyadav56@gmail.com>
2026-06-16 10:43:29 +0530

    chore: remove patr_mobile directory

--- patr_mobile/android/app/src/main/kotlin/com/patr/patr_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.patr.patr_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- patr_mobile/test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('App smoke test', (WidgetTester tester) async {
-    // Placeholder test to satisfy flutter test compilation
-    expect(true, isTrue);
-  });
-}
